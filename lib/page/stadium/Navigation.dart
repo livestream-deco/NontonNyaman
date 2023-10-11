0bfea7f07f1acd3c943741333c3cf6a7964a5962
reynaldi0810 <reynaldi.oktavianus8@gmail.com>
2023-10-11 22:48:39 +1000

    fix navigation

--- lib/page/stadium/Navigation.dart
+++ lib/page/stadium/Navigation.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names, library_private_types_in_public_api, empty_catches, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location_permissions/location_permissions.dart';
